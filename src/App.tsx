d5935df42507e3f05050f4d66bdeb0bee2ab19f7
Jim08201200 <hongjm@changmeng.com>
2025-12-03 19:29:20 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -399,7 +399,7 @@ const Experience = ({ sceneState, rotationSpeed }: { sceneState: 'CHAOS' | 'FORM
       <pointLight position={[-30, 10, -30]} intensity={50} color={CONFIG.colors.gold} />
       <pointLight position={[0, -20, 10]} intensity={30} color="#ffffff" />
 
-      <group position={[0, -6, 0]}>
+      <group position={[0, -1, 0]}>
         <Foliage state={sceneState} />
         <Suspense fallback={null}>
            <PhotoOrnaments state={sceneState} />
